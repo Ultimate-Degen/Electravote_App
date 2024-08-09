466a9fa6fd2edff4873abfe672707f5c8c9ae6a2
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-09 14:28:13 +0200

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express();
 
+
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
