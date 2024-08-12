b042c5d7686b32cef282de839d9153279479cd55
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-12 16:02:05 +0200

    modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,49 +1,3 @@
-/*const express = require('express');
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
-const path = require('path');
-const ejs = require('ejs');
-
-const app = express();
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Serve static files from the correct directory
-app.use('/css', express.static(path.join(__dirname, '../../public/css')));
-app.use('/images', express.static(path.join(__dirname, '../../public/images')));
-app.use('/js', express.static(path.join(__dirname, '../../public/js')));
-
-// View Engine - Corrected path to views directory
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));  // This should correctly point to the views directory
-
-// Routes - Adjusted to reflect correct location of routes in netlify directory
-const indexRoute = require('../routes/index');
-const createElectionRoute = require('../routes/create-election');
-const votingRoute = require('../routes/voting');
-const resultsRoute = require('../routes/results');
-const myElectionsRoute = require('../routes/my-elections');
-const votingHistoryRoute = require('../routes/voting-history');
-const profileRoute = require('../routes/profile');
-const settingsRoute = require('../routes/settings');
-const dashboardRoute = require('../routes/dashboard');
-const notificationsRoute = require('../routes/notifications');
-
-// Use routes
-app.use('/', indexRoute);
-app.use('/create-election', createElectionRoute);
-app.use('/voting', votingRoute);
-app.use('/results', resultsRoute);
-app.use('/my-elections', myElectionsRoute);
-app.use('/voting-history', votingHistoryRoute);
-app.use('/profile', profileRoute);
-app.use('/settings', settingsRoute);
-app.use('/dashboard', dashboardRoute);
-app.use('/notifications', notificationsRoute);
-
-module.exports.handler = serverless(app);*/
-
 const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
@@ -60,11 +14,11 @@ app.use('/css', express.static(path.join(__dirname, '../../public/css')));
 app.use('/images', express.static(path.join(__dirname, '../../public/images')));
 app.use('/js', express.static(path.join(__dirname, '../../public/js')));
 
-// View Engine - Corrected path to views directory
+// View Engine - Set path to the views directory now inside netlify/views
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));  // This should correctly point to the views directory
+app.set('views', path.join(__dirname, '../views'));  // ../views because views is in netlify, server.js is in netlify/functions
 
-// Routes - Adjusted to reflect correct location of routes in netlify directory
+// Routes
 const indexRoute = require('../routes/index');
 const createElectionRoute = require('../routes/create-election');
 const votingRoute = require('../routes/voting');
@@ -76,7 +30,6 @@ const settingsRoute = require('../routes/settings');
 const dashboardRoute = require('../routes/dashboard');
 const notificationsRoute = require('../routes/notifications');
 
-// Use routes
 app.use('/', indexRoute);
 app.use('/create-election', createElectionRoute);
 app.use('/voting', votingRoute);
