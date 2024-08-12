5c62c79338c4b4e6ff3dab89272611851ae486e9
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-12 15:08:19 +0200

    modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -10,25 +10,25 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static files from the correct directory
-app.use('/css', express.static(path.join(__dirname, '../../public/css')));
-app.use('/images', express.static(path.join(__dirname, '../../public/images')));
-app.use('/js', express.static(path.join(__dirname, '../../public/js')));
+app.use('/css', express.static(path.join(__dirname, '../public/css')));
+app.use('/images', express.static(path.join(__dirname, '../public/images')));
+app.use('/js', express.static(path.join(__dirname, '../public/js')));
 
-// View Engine - Set path to the views directory now inside netlify/functions
+// View Engine - Set path to the views directory now inside netlify
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));  // Views path adjusted to reflect new location
+app.set('views', path.join(__dirname, '../views'));  // Views path adjusted to reflect new location in netlify directory
 
-// Routes - Adjusted to reflect new location
-const indexRoute = require('./routes/index');
-const createElectionRoute = require('./routes/create-election');
-const votingRoute = require('./routes/voting');
-const resultsRoute = require('./routes/results');
-const myElectionsRoute = require('./routes/my-elections');
-const votingHistoryRoute = require('./routes/voting-history');
-const profileRoute = require('./routes/profile');
-const settingsRoute = require('./routes/settings');
-const dashboardRoute = require('./routes/dashboard');
-const notificationsRoute = require('./routes/notifications');
+// Routes - Adjusted to reflect new location in netlify directory
+const indexRoute = require('../routes/index');
+const createElectionRoute = require('../routes/create-election');
+const votingRoute = require('../routes/voting');
+const resultsRoute = require('../routes/results');
+const myElectionsRoute = require('../routes/my-elections');
+const votingHistoryRoute = require('../routes/voting-history');
+const profileRoute = require('../routes/profile');
+const settingsRoute = require('../routes/settings');
+const dashboardRoute = require('../routes/dashboard');
+const notificationsRoute = require('../routes/notifications');
 
 // Use routes
 app.use('/', indexRoute);
