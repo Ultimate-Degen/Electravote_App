640f65473d391a1bbcdd218f3ca8472ee0ade830
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-12 11:10:05 +0200

    modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -161,7 +161,7 @@ console.log('Current directory:', __dirname);
 
 module.exports.handler = serverless(app);*/
 
-const express = require('express');
+/*const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -199,4 +199,46 @@ app.use('./views/settings', settingsRoute);
 app.use('./views/dashboard', dashboardRoute);
 app.use('./views/notifications', notificationsRoute);
 
+module.exports.handler = serverless(app);*/
+
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+// Routes
+const indexRoute = require('./routes/index');
+const createElectionRoute = require('./routes/create-election');
+const votingRoute = require('./routes/voting');
+const resultsRoute = require('./routes/results');
+const myElectionsRoute = require('./routes/my-elections');
+const votingHistoryRoute = require('./routes/voting-history');
+const profileRoute = require('./routes/profile');
+const settingsRoute = require('./routes/settings');
+const dashboardRoute = require('./routes/dashboard');
+const notificationsRoute = require('./routes/notifications');
+
+// Corrected paths for the routes
+app.use('/', indexRoute);
+app.use('/create-election', createElectionRoute);
+app.use('/voting', votingRoute);
+app.use('/results', resultsRoute);
+app.use('/my-elections', myElectionsRoute);
+app.use('/voting-history', votingHistoryRoute);
+app.use('/profile', profileRoute);
+app.use('/settings', settingsRoute);
+app.use('/dashboard', dashboardRoute);
+app.use('/notifications', notificationsRoute);
+
 module.exports.handler = serverless(app);
+
