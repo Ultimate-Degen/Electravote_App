9400aa422ee2498cc28cb84a878750d89f3b1d38
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-12 15:57:52 +0200

    modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -52,13 +52,40 @@ const ejs = require('ejs');
 
 const app = express();
 
-console.log("Current directory:", __dirname);
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Serve static files from the correct directory
+app.use('/css', express.static(path.join(__dirname, '../../public/css')));
+app.use('/images', express.static(path.join(__dirname, '../../public/images')));
+app.use('/js', express.static(path.join(__dirname, '../../public/js')));
+
+// View Engine - Corrected path to views directory
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../views'));  // This should correctly point to the views directory
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Test' });
-});
+// Routes - Adjusted to reflect correct location of routes in netlify directory
+const indexRoute = require('../routes/index');
+const createElectionRoute = require('../routes/create-election');
+const votingRoute = require('../routes/voting');
+const resultsRoute = require('../routes/results');
+const myElectionsRoute = require('../routes/my-elections');
+const votingHistoryRoute = require('../routes/voting-history');
+const profileRoute = require('../routes/profile');
+const settingsRoute = require('../routes/settings');
+const dashboardRoute = require('../routes/dashboard');
+const notificationsRoute = require('../routes/notifications');
+
+// Use routes
+app.use('/', indexRoute);
+app.use('/create-election', createElectionRoute);
+app.use('/voting', votingRoute);
+app.use('/results', resultsRoute);
+app.use('/my-elections', myElectionsRoute);
+app.use('/voting-history', votingHistoryRoute);
+app.use('/profile', profileRoute);
+app.use('/settings', settingsRoute);
+app.use('/dashboard', dashboardRoute);
+app.use('/notifications', notificationsRoute);
 
 module.exports.handler = serverless(app);
