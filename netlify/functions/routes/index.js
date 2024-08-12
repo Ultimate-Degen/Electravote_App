3613506d92d08a264869b1c549c1d3271c10fab2
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-12 14:27:28 +0200

    modified:   netlify/functions/routes/index.js

--- netlify/functions/routes/index.js
+++ netlify/functions/routes/index.js
@@ -1,34 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-//router.get('/', (req, res) => {
-//    res.render('index.ejs');
-//});
-
-//module.exports = router;
-
 router.get('/', (req, res) => {
-    res.send(`
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>ElectraVote - Home</title>
-      </head>
-      <body>
-          <h1>Welcome to ElectraVote</h1>
-          <nav>
-              <ul>
-                  <li><a href="/create-election">Create Election</a></li>
-                  <li><a href="/voting">Voting</a></li>
-                  <li><a href="/results">Results</a></li>
-                  <!-- Add more navigation items as needed -->
-              </ul>
-          </nav>
-      </body>
-      </html>
-    `);
-  });
-  
-  module.exports = router;
+    res.render('index.ejs');
+});
+
+module.exports = router;
+
