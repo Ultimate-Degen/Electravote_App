652f6a60b493cbb618ca157e9797a85f290681bf
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-13 14:39:23 +0200

    new file:   netlify.toml 	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
+// Adjusted paths for Netlify Functions
+const viewPath = path.join(__dirname, '../../views');
+const staticPath = path.join(__dirname, '../../public');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render(path.join(viewPath, 'index'));
 });
 
 module.exports = router;
