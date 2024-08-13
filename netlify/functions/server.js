61bd6e44355fc2b450e5c6d35cd6a2bc7350032b
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-13 15:39:39 +0200

    modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '../../public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../../views'));
 
-// Routes
+// Import routes
 const indexRoute = require('../../routes/index');
 const createElectionRoute = require('../../routes/create-election');
 const votingRoute = require('../../routes/voting');
@@ -23,15 +23,16 @@ const settingsRoute = require('../../routes/settings');
 const dashboardRoute = require('../../routes/dashboard');
 const notificationsRoute = require('../../routes/notifications');
 
+// Route handling
 app.use('/.netlify/functions/server/', indexRoute);
-app.use('/.netlify/functions/server/create-election', require('../../routes/create-election'));
-app.use('/.netlify/functions/server/voting', require('../../routes/voting'));
-app.use('/.netlify/functions/server/results', require('../../routes/results'));
-app.use('/.netlify/functions/server/my-elections', require('../../routes/my-ElectionsRoute'));
-app.use('/.netlify/functions/server/voting-history', require ('../../routes/voting-HistoryRoute'));
-app.use('/.netlify/functions/server/profile', require('../../routes/ProfileRoute'));
-app.use('/.netlify/functions/server/settings', require('../../routes/settingsRoute'));
-app.use('/.netlify/functions/server/dashboard', require('../../routes/dashboardRoute'));
-app.use('/.netlify/functions/server/notifications', require('../../routes/notificationsRoute'));
+app.use('/.netlify/functions/server/create-election', createElectionRoute);
+app.use('/.netlify/functions/server/voting', votingRoute);
+app.use('/.netlify/functions/server/results', resultsRoute);
+app.use('/.netlify/functions/server/my-elections', myElectionsRoute);
+app.use('/.netlify/functions/server/voting-history', votingHistoryRoute);
+app.use('/.netlify/functions/server/profile', profileRoute);
+app.use('/.netlify/functions/server/settings', settingsRoute);
+app.use('/.netlify/functions/server/dashboard', dashboardRoute);
+app.use('/.netlify/functions/server/notifications', notificationsRoute);
 
 module.exports.handler = serverless(app);
