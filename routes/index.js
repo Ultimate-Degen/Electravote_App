e47c453bbb27020060010342967fdb17b745a439
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-13 16:50:13 +0200

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public", "index"))
+    res.sendFile(path.join(__dirname, "../public", "index.ejs"))
 });
 
 module.exports = router;
