620d265c3fcb1a11c74a2f8e1da1b76a97f114de
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-13 17:11:07 +0200

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views", "index.ejs"));
+    res.render('index');  // Changed from sendFile to render
 });
-
 module.exports = router;
