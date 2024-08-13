b470c9192b43f28d9a080a0ac21f792153d29a1d
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-13 16:30:35 +0200

    modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -7,24 +7,20 @@ const app = express();
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, '../../public')));
 
-// View Engine setup
+// Set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../../views'));
 
-// Root route
-const indexRoute = require('../../routes/index');
-
-app.use('/.netlify/functions/server/', indexRoute); // This handles the root URL
+// Define a simple root route to test
+app.get('/.netlify/functions/server/', (req, res) => {
+    res.render('index'); // Render the index.ejs view
+});
 
 // Other routes
 app.use('/.netlify/functions/server/create-election', require('../../routes/create-election'));
 app.use('/.netlify/functions/server/voting', require('../../routes/voting'));
 app.use('/.netlify/functions/server/results', require('../../routes/results'));
-app.use('/.netlify/functions/server/my-elections', require('../../routes/my-elections'));
-app.use('/.netlify/functions/server/voting-history', require('../../routes/voting-history'));
-app.use('/.netlify/functions/server/profile', require('../../routes/profile'));
-app.use('/.netlify/functions/server/settings', require('../../routes/settings'));
-app.use('/.netlify/functions/server/dashboard', require('../../routes/dashboard'));
-app.use('/.netlify/functions/server/notifications', require('../../routes/notifications'));
+// Add other routes as needed
 
+// Export the serverless function
 module.exports.handler = serverless(app);
