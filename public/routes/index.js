4047bae59bcd5b949fbf2b50a4c6145992b82345
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-14 11:37:09 +0200

    deleted:    public/routes/create-election.js 	deleted:    public/routes/dashboard.js 	deleted:    public/routes/index.js 	deleted:    public/routes/my-elections.js 	deleted:    public/routes/notifications.js 	deleted:    public/routes/profile.js 	deleted:    public/routes/results.js 	deleted:    public/routes/settings.js 	deleted:    public/routes/voting-history.js 	deleted:    public/routes/voting.js 	deleted:    public/views/create-election.ejs 	deleted:    public/views/dashboard.ejs 	deleted:    public/views/election-success.ejs 	deleted:    public/views/index.ejs 	deleted:    public/views/my-elections.ejs 	deleted:    public/views/notifications.ejs 	deleted:    public/views/profile-success.ejs 	deleted:    public/views/profile.ejs 	deleted:    public/views/results.ejs 	deleted:    public/views/settings-success.ejs 	deleted:    public/views/settings.ejs 	deleted:    public/views/vote-success.ejs 	deleted:    public/views/voting-history.ejs 	deleted:    public/views/voting.ejs

--- public/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-//const path = require('path');
-
-router.get('/', (req, res) => {
-    res.render('index');  // Changed from sendFile to render
-});
-module.exports = router;
