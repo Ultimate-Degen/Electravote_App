6d2b69deb7dd234e457cea1e2a60cfa32a0bdb26
Ultimate-Degen <angelathe90sbaby@gmail.com>
2024-08-14 10:43:57 +0200

    modified:   netlify/functions/server.js 	modified:   package.json

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,19 +2,22 @@ const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
 const app = express();
-const ejs = require('ejs');
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../../public')));
 
 // Set up view engine with absolute path
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set('views', path.join(__dirname, '../../views'));
 
 // Root route to render the index.ejs view
 app.get('/.netlify/functions/server/', (req, res) => {
     res.render('index');  // Rendering the index.ejs view
+});
+
+// Example of a secondary route
+app.get('/.netlify/functions/server/test', (req, res) => {
+    res.send('Test route is working!');
 });
 
 // Catch-all route for handling unknown paths
